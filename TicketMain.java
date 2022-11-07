30b51386ee7a9ce051b31b8a988ed3688e7b6fe4
Zane Sanchez <74287373+DivinityDistilled@users.noreply.github.com>
2022-11-06 22:20:36 -0600

    Update TicketMain.java

--- TicketMain.java
+++ TicketMain.java
@@ -17,10 +17,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class TicketMain extends AppCompatActivity{
 
         public class Ticket{                    // Ticket Object
-                private double ticket_price;
-                private String route;
-                private double day;
-                private double time;
+                private double ticket_price;    // Ticket Price
+                private String route;           // Ticket Route
+                private double day;             // Day for Ticket
+                private double time;            // Time for Train Deperature
 
                 public Ticket(double price, String routeName, double date, double timer){       // Constructor for Ticket
                         ticket_price = price;
@@ -28,12 +28,12 @@ public class TicketMain extends AppCompatActivity{
                         day = date;
                         time = timer;
                 }
-                // Accessors
+                // Accessors for Ticket
                 public double getPrice(){ return ticket_price;}
                 public String getRoute(){ return route;}
                 public double getDay(){ return day;}
                 public double getTime(){ return time;}
-                // Mutators
+                // Mutators for Ticket
                 public void editPrice(double price) {  ticket_price = price;}
                 public void editRoute(String Route) {  route = Route;}
                 public void editDay(double newDay) {  day = newDay;}
